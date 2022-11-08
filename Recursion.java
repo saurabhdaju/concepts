79de1bc3a2bcc9867c44d25d1cf7d4fed1b85292
saurabhdaju <saurabhpandey8707@gmail.com>
2022-11-08 18:24:58 +0530

    rotated binary search

--- Recursion.java
+++ Recursion.java
@@ -5,6 +5,7 @@ package Concepts;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Scanner;
 
 class SubsetsOfString {
     public static void main(String[] args) {
@@ -215,3 +216,33 @@ class LinearSearch {
     }
 }
 
+class RotatedBinarySearch {
+    public static void main(String[] args) {
+        int[] arr = {6, 7, 8, 10, 1, 2, 3, 4};
+        int target = 5;
+
+        int ans = search(arr, target, 0, arr.length - 1);
+
+        System.out.println(ans);
+    }
+
+    static int search(int[] arr, int target, int start, int end) {
+        if(start > end) return -1;
+
+        int mid = start + (end - start) / 2;
+
+        if(arr[mid] == target) return mid;
+
+        if(arr[start] <= arr[mid]) {
+            if(target >= arr[start] && target < arr[mid]) return search(arr, target, start, mid - 1);
+            else return search(arr, target, mid + 1, end);
+        }
+
+        else {
+            if(target >= arr[start]) {
+                return search(arr, target, start, mid - 1);
+            }
+            else return search(arr, target, mid + 1, end);
+        }
+    }
+}
